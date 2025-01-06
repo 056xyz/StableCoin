e7ba6a3dc020bc3bdd9305956200f6ab2a6ff163
abgnv <bogunovaleksandar@gmail.com>
2025-01-06 23:52:04 +0200

    initial fuzz tests suite

---
+++ test/fuzz/InvariantsTest.t.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.22;
+
+import {Test, console} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {Handler} from "../fuzz/Handler.t.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+// Invariants:
+// protocol must never be insolvent / undercollateralized
+// TODO: users cant create stablecoins with a bad health factor
+// TODO: a user should only be able to be liquidated if they have a bad health factor
+// Getter view functions should never revert
+
+contract InvariantsTest is StdInvariant, Test {
+    DeployDSC deployer;
+    DSCEngine dsce;
+    DecentralizedStableCoin dsc;
+    HelperConfig config;
+    Handler handler;
+    address weth;
+    address wbtc;
+
+    function setUp() external {
+        deployer = new DeployDSC();
+        (dsc, dsce, config) = deployer.run();
+        (,, weth, wbtc,) = config.activeNetworkConfig();
+        handler = new Handler(dsce, dsc);
+        targetContract(address(handler));
+    }
+
+    function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
+        uint256 totalSupply = dsc.totalSupply();
+        uint256 totalWethDeposited = IERC20(weth).balanceOf(address(dsce));
+        uint256 totalWbtcDeposited = IERC20(wbtc).balanceOf(address(dsce));
+
+        uint256 wethValue = dsce.getUsdValue(weth, totalWethDeposited);
+        uint256 wbtcValue = dsce.getUsdValue(wbtc, totalWbtcDeposited);
+
+        console.log("totalSupply: ", totalSupply);
+        console.log("wethValue: ", wethValue);
+        console.log("wbtcValue: ", wbtcValue);
+
+        assert(wethValue + wbtcValue >= totalSupply);
+    }
+
+    function invariant_gettersCantRevert() public view {
+        dsce.getAdditionalFeedPrecision();
+        dsce.getCollateralTokens();
+        dsce.getLiquidationBonus();
+        dsce.getLiquidationThreshold();
+        dsce.getMinHealthFactor();
+        dsce.getPrecision();
+        dsce.getDsc();
+        // dsce.getTokenAmountFromUsd();
+        // dsce.getCollateralTokenPriceFeed();
+        // dsce.getCollateralBalanceOfUser();
+        // getAccountCollateralValue();
+    }
+}
